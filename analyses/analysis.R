a16ac26bcf6f05a4772369076d55881b219da96e
justinek <justinek@stanford.edu>
2015-01-22 15:16:43 -0800

    added figure sand revised experiment 2

--- analyses/analysis.R
+++ analyses/analysis.R
@@ -27,7 +27,7 @@ priors.states$imageID.reordered <- factor(priors.states$imageID, levels=c(3, 2, 
                                                    "Weather context 7", "Weather context 8", "Weather context 9"))
 
 priors.states$imageCategory <- ifelse(as.numeric(priors.states$imageID) <= 3, "Positive", 
-                                      ifelse(as.numeric(priors.states$imageID) >=7,
+                                      ifelse(as.numeric(priors.states$imageID) >= 7,
                                                                                      "Negative", "Neutral"))
 priors.states$imageCategory <- factor(priors.states$imageCategory, levels=c("Positive", "Neutral", "Negative"))
 ggplot(priors.states, aes(x=stateRating, y=prior)) +
@@ -51,6 +51,7 @@ ggplot(priors.states, aes(x=stateRating, y=prior, color=imageID)) +
 # affect ratings
 
 comps <- princomp(data=priors, ~ sad + disgusted + angry + neutral + content + happy + excited, cor=FALSE, scores=TRUE)
+#comps <- princomp(data=priors, ~ sad + disgusted + angry + content + happy + excited, cor=FALSE, scores=TRUE)
 summary(comps)
 biplot(comps)
 
@@ -210,6 +211,54 @@ affect.pca.long <-  melt(data=affect.pca, id.vars=c("workerID", "gender", "age",
 affect.pca.long$value.corrected <- ifelse(affect.pca.long$variable == "Comp.1", affect.pca.long$value, 
                                           1 - affect.pca.long$value)
 
+###############
+# Split half
+############### 
+splithalf.cors <- data.frame(valence=NULL, arousal=NULL)
+t = 1
+while (t <= 1000) {
+  ii <- seq_len(nrow(affect.pca))
+  ind1 <- sample(ii, nrow(affect.pca) / 2) 
+  ind2 <- ii[!ii %in% ind1] 
+  h1 <- affect.pca[ind1, ] 
+  h2 <- affect.pca[ind2, ]
+  h1.valence.summary <- summarySE(h1, measurevar="Comp.1", groupvars=c("imageID", "utterance"))
+  h1.arousal.summary <- summarySE(h1, measurevar="Comp.2", groupvars=c("imageID", "utterance"))
+  h2.valence.summary <- summarySE(h2, measurevar="Comp.1", groupvars=c("imageID", "utterance"))
+  h2.arousal.summary <- summarySE(h2, measurevar="Comp.2", groupvars=c("imageID", "utterance"))
+  colnames(h1.valence.summary)[4] <- "valence1"
+  colnames(h2.valence.summary)[4] <- "valence2"
+  colnames(h1.arousal.summary)[4] <- "arousal1"
+  colnames(h2.arousal.summary)[4] <- "arousal2"
+  if (nrow(h1.valence.summary) == nrow(h2.valence.summary) & nrow(h1.arousal.summary) == nrow(h2.arousal.summary)) {
+    splithalf.valence <- join(h1.valence.summary, h2.valence.summary, by=c("imageID", "utterance"))
+    splithalf.arousal <- join(h1.arousal.summary, h2.arousal.summary, by=c("imageID", "utterance"))
+    t <- t+1
+    valence.cor <- with(splithalf.valence, cor(valence1, valence2))
+    arousal.cor <- with(splithalf.arousal, cor(arousal1, arousal2))
+    this.frame <- data.frame(valence=valence.cor,
+                             arousal=arousal.cor)
+    splithalf.cors <- rbind(splithalf.cors, this.frame)
+  }
+}
+
+prophet <- function(reliability, length) {
+  prophecy <- length * reliability / (1 + (length - 1)*reliability)
+  return (prophecy)
+}
+
+splithalf.cors <- splithalf.cors[complete.cases(splithalf.cors),]
+
+splithalf.cors$valence.proph <- prophet(splithalf.cors$valence, 2)
+splithalf.cors$arousal.proph <- prophet(splithalf.cors$arousal, 2)
+
+valence.proph.summary  <- summarySE(data=splithalf.cors, measurevar="valence.proph", groupvars=NULL)
+arousal.proph.summary  <- summarySE(data=splithalf.cors, measurevar="arousal.proph", groupvars=NULL)
+
+######################
+# affect
+#######################
+
 
 affect.pca.summary <- summarySE(affect.pca.long, measurevar="value.corrected",
                                 groupvars=c("imageID", "utterance", "variable"))
@@ -296,10 +345,13 @@ comp.state <- join(interp.states, model.state, by=c("imageID", "utterance", "sta
 with(comp.state, cor.test(prob, probability))
 comp.state$literal <- ifelse(comp.state$state==comp.state$utterance, "literal", "non")
 
-ggplot(comp.state, aes(x=probability, y=prob, color=literal)) +
+ggplot(comp.state, aes(x=probability, y=prob)) +
   #geom_text(aes(label=state)) +
-  geom_point() +
+  geom_point(color="black") +
   #facet_grid(imageID ~ utterance) +
+  geom_smooth(method=lm, color="gray") +
+  #ylim(c(0, 1)) +
+  #xlim(c(0, 1)) +
   theme_bw() +
   xlab("Model") +
   ylab("Humans")
@@ -311,14 +363,18 @@ interp.states$imageID.utterance <- NULL
 comp.state.long <- rbind(interp.states, model.state)
 comp.state.long$imageID.reordered <- factor(comp.state.long$imageID, levels=c(3, 2, 1, 6, 5, 4, 8, 7, 9),
                                                     labels=c("w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9"))
-ggplot(comp.state.long, aes(x=state, y=prob, color=type)) +
+comp.state.long$imageCategory <- ifelse(as.numeric(comp.state.long$imageID) <= 3, "Positive", 
+                                      ifelse(as.numeric(comp.state.long$imageID) >= 7,
+                                             "Negative", "Neutral"))
+comp.state.long$imageCategory <- factor(comp.state.long$imageCategory, levels=c("Positive", "Neutral", "Negative"))
+ggplot(comp.state.long, aes(x=state, y=prob, color=imageCategory)) +
   geom_point(size=3) +
-  geom_line(aes(group=type)) +
+  geom_line(aes(group=type, linetype=type)) +
   facet_grid(imageID.reordered ~ utterance) +
   theme_bw() +
-  scale_color_manual(values=c("#003366", "#3399ff"), name="") +
-  scale_linetype_discrete(legend=NULL) +
-  ylab("Probability of weather state given utterance") +
+  scale_color_manual(values=c("#fc4e2a", "#99d8c9", "#2b8cbe"), name="Weather type") +
+  scale_linetype_discrete(name="Interpretation") +
+  ylab("Probability") +
   xlab("Weather state") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
   
@@ -334,7 +390,8 @@ ggplot(comp.valence, aes(x=probability, y=value.corrected)) +
   geom_point() +
   theme_bw() +
   xlab("Model") +
-  ylab("Humans") 
+  ylab("Humans") +
+  geom_smooth(method=lm, color="gray") 
 
 #####
 # Arousal
@@ -343,13 +400,31 @@ interp.arousal <- subset(affect.pca.summary, variable=="Comp.2")
 model.arousal.high <- subset(model.arousal, arousal=="high")
 comp.arousal <- join(interp.arousal, model.arousal.high, by=c("imageID", "utterance"))
 with(comp.arousal, cor.test(value.corrected, probability))
-ggplot(comp.arousal, aes(x=probability, y=value.corrected, color=utterance)) +
+ggplot(comp.arousal, aes(x=probability, y=value.corrected)) +
   geom_point() +
   theme_bw() +
   #facet_wrap(~imageID, ncol=3) +
   xlab("Model") +
-  ylab("Humans")
-  
+  ylab("Humans") +
+  geom_smooth(method=lm, color="gray")
+
+##################
+# Combined scatter plot
+###################
+comp.state$interp <- "state"
+comp.valence$interp <- "valence"
+comp.arousal$interp <- "arousal"
+comp.all <- data.frame(interp=c(comp.state$interp, comp.valence$interp, comp.arousal$interp),
+                       imageID=c(comp.state$imageID, comp.valence$imageID, comp.arousal$imageID),
+                       human=c(comp.state$prob, comp.valence$value.corrected, comp.arousal$value.corrected),
+                       model=c(comp.state$probability, comp.valence$probability, comp.arousal$probability))
+comp.all$interp <- factor(comp.all$interp, levels=c("state", "valence", "arousal"))
+ggplot(comp.all, aes(x=model, y=human)) +
+  geom_point() +
+  geom_smooth(method=lm, color="grey") +
+  facet_grid(interp~.) +
+  theme_bw() +
+  ylim(c(0, 1))
 
 #########################
 # Add priors
@@ -383,23 +458,46 @@ priors.affect.byImage.summary <- summarySE(priors.pca.probit.long, measurevar="v
                                        groupvars=c("variable", "imageID"))
 
 priors.valence <- subset(priors.affect.byImage.summary, variable=="Comp.1")
-comp.prior.valence <- join(priors.valence, interp.valence, by="imageID")
+priors.valence$N <- NULL
+priors.valence$sd <- NULL
+priors.valence$se <- NULL
+priors.valence$ci <- NULL
+
+interp.valence <- rename(interp.valence, replace=c("value.corrected"="probability"))
+interp.valence$N <- NULL
+interp.valence$sd <- NULL
+interp.valence$se <- NULL
+interp.valence$type <- "human"
+model.valence.pos$variable <- "Comp.1"
+model.valence.pos$valence <- NULL
+model.valence.pos$ci <- 0
+model.valence.pos$type <- "model"
+comp.valence.long <- rbind(interp.valence, model.valence.pos)
+
+comp.prior.valence <- join(priors.valence, comp.valence.long, by="imageID")
 comp.prior.valence$utterance <- factor(comp.prior.valence$utterance, levels=c("prior", "terrible", "bad",
                                                                           "neutral", "good", "amazing"))
 comp.prior.valence$imageID.reordered <- factor(comp.prior.valence$imageID, levels=c(3, 2, 1, 6, 5, 4, 8, 7, 9),
-                                          labels=c("w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9"))
+                                          labels=c("Weather context 1", "Weather context 2", "Weather context 3", 
+                                                   "Weather context 4", "Weather context 5", "Weather context 6", 
+                                                   "Weather context 7", "Weather context 8", "weather contextw 9"))
 
-colnames(comp.prior.valence)[4] <- "prior"
+colnames(comp.prior.valence)[3] <- "prior"
 
-ggplot(comp.prior.valence, aes(x=utterance, y=value.corrected)) +
+comp.prior.valence$imageCategory <- ifelse(as.numeric(comp.prior.valence$imageID) <= 3, "Positive", 
+                                        ifelse(as.numeric(comp.prior.valence$imageID) >= 7,
+                                               "Negative", "Neutral"))
+comp.prior.valence$imageCategory <- factor(comp.prior.valence$imageCategory, levels=c("Positive", "Neutral", "Negative"))
+ggplot(comp.prior.valence, aes(x=utterance, y=probability, color=imageCategory)) +
   geom_point() +
-  geom_line(aes(group=imageID.reordered)) +
+  geom_line(aes(group=type, linetype=type)) +
   geom_hline(aes(yintercept=prior), color="grey", linetype=2) +
   #geom_bar(stat="identity", color="black", fill="grey") +
-  geom_errorbar(aes(ymin=value.corrected-ci, ymax=value.corrected+ci), width=0.05) +
+  geom_errorbar(aes(ymin=probability-ci, ymax=probability+ci), width=0.05) +
   theme_bw() +
   facet_wrap(~imageID.reordered, ncol=3) +
-  scale_color_manual(values=c("gray", colors.utterances)) +
+  scale_color_manual(values=c("#fc4e2a", "#99d8c9", "#2b8cbe"), name="Weather type", guide=FALSE) +
+  scale_linetype_discrete(name="Interpretation", labels=c("Human", "Model"), guide=FALSE) +
   xlab("Utterance") +
   ylab("Probability of positive valence")
 
@@ -407,6 +505,50 @@ ggplot(comp.prior.valence, aes(x=utterance, y=value.corrected)) +
 # Arousal
 #####
 priors.arousal <- subset(priors.affect.byImage.summary, variable=="Comp.2")
+
+priors.arousal$N <- NULL
+priors.arousal$sd <- NULL
+priors.arousal$se <- NULL
+priors.arousal$ci <- NULL
+
+interp.arousal <- rename(interp.arousal, replace=c("value.corrected"="probability"))
+interp.arousal$N <- NULL
+interp.arousal$sd <- NULL
+interp.arousal$se <- NULL
+interp.arousal$type <- "human"
+model.arousal.high$variable <- "Comp.2"
+model.arousal.high$arousal <- NULL
+model.arousal.high$ci <- 0
+model.arousal.high$type <- "model"
+comp.arousal.long <- rbind(interp.arousal, model.arousal.high)
+
+comp.prior.arousal <- join(priors.arousal, comp.arousal.long, by="imageID")
+comp.prior.arousal$utterance <- factor(comp.prior.arousal$utterance, levels=c("prior", "terrible", "bad",
+                                                                              "neutral", "good", "amazing"))
+comp.prior.arousal$imageID.reordered <- factor(comp.prior.arousal$imageID, levels=c(3, 2, 1, 6, 5, 4, 8, 7, 9),
+                                               labels=c("Weather context 1", "Weather context 2", "Weather context 3", 
+                                                        "Weather context 4", "Weather context 5", "Weather context 6", 
+                                                        "Weather context 7", "Weather context 8", "weather contextw 9"))
+
+colnames(comp.prior.arousal)[3] <- "prior"
+
+comp.prior.arousal$imageCategory <- ifelse(as.numeric(comp.prior.arousal$imageID) <= 3, "Positive", 
+                                           ifelse(as.numeric(comp.prior.arousal$imageID) >= 7,
+                                                  "Negative", "Neutral"))
+comp.prior.arousal$imageCategory <- factor(comp.prior.arousal$imageCategory, levels=c("Positive", "Neutral", "Negative"))
+ggplot(comp.prior.arousal, aes(x=utterance, y=probability, color=imageCategory)) +
+  geom_point() +
+  geom_line(aes(group=type, linetype=type)) +
+  geom_hline(aes(yintercept=prior), color="grey", linetype=2) +
+  #geom_bar(stat="identity", color="black", fill="grey") +
+  geom_errorbar(aes(ymin=probability-ci, ymax=probability+ci), width=0.05) +
+  theme_bw() +
+  facet_wrap(~imageID.reordered, ncol=3) +
+  scale_color_manual(values=c("#fc4e2a", "#99d8c9", "#2b8cbe"), name="Weather type") +
+  scale_linetype_discrete(name="Interpretation", labels=c("Human", "Model")) +
+  xlab("Utterance") +
+  ylab("Probability of high arousal")
+
 comp.prior.arousal <- join(priors.arousal, interp.arousal, by="imageID")
 comp.prior.arousal$utterance <- factor(comp.prior.arousal$utterance, levels=c("prior", "terrible", "bad",
                                                                               "neutral", "good", "amazing"))
@@ -414,6 +556,8 @@ comp.prior.arousal$imageID.reordered <- factor(comp.prior.arousal$imageID, level
                                                labels=c("w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9"))
 
 colnames(comp.prior.arousal)[4] <- "prior"
+
+
 
 ggplot(comp.prior.arousal, aes(x=utterance, y=value.corrected)) +
   geom_point() +
